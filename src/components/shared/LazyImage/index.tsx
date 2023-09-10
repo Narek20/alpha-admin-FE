1cc0ff461a0d9c8642d387edbb5625cce61fee5f
Narek Hovhannisyan <n.hovo@bk.ru>
2023-09-10 13:02:07 +0400

    added lazy loading styles

--- src/components/shared/LazyImage/index.tsx
+++ src/components/shared/LazyImage/index.tsx
@@ -1,58 +1,57 @@
-import { CSSProperties, FC, useEffect, useState, useRef } from 'react';
-
+import { CSSProperties, FC, useEffect, useState, useRef } from 'react'
+import styles from './styles.module.scss'
 interface IProps {
   src: string
   alt?: string
   styles?: CSSProperties
 }
 
-const  LazyImage:FC<IProps> = ({ src, alt, styles }) => {
-  const imgRef = useRef<HTMLImageElement>(null);
-  const [imageLoaded, setImageLoaded] = useState(false);
+const LazyImage: FC<IProps> = ({ src, alt, styles }) => {
+  const imgRef = useRef<HTMLImageElement>(null)
+  const [imageLoaded, setImageLoaded] = useState(false)
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting && imgRef.current) {
           // Load the image when it becomes visible
-          imgRef.current.src = src;
-          observer.unobserve(imgRef.current);
+          imgRef.current.src = src
+          observer.unobserve(imgRef.current)
         }
-      });
-    });
+      })
+    })
 
     // Start observing the image element
     if (imgRef.current) {
-      observer.observe(imgRef.current);
+      observer.observe(imgRef.current)
     }
 
     // Handle image load event
     const handleImageLoad = () => {
-      setImageLoaded(true);
-    };
+      setImageLoaded(true)
+    }
 
     if (imgRef.current) {
-      imgRef.current.addEventListener('load', handleImageLoad);
+      imgRef.current.addEventListener('load', handleImageLoad)
     }
 
     return () => {
       if (imgRef.current) {
-        observer.unobserve(imgRef.current);
-        imgRef.current.removeEventListener('load', handleImageLoad);
+        observer.unobserve(imgRef.current)
+        imgRef.current.removeEventListener('load', handleImageLoad)
       }
-    };
-  }, [src]);
-
+    }
+  }, [src])
 
   return (
     <img
       ref={imgRef}
-      className="lazy-image"
+      className={`lazy-image ${imageLoaded ? 'loaded' : ''}`}
       style={styles}
       src={src}
       alt={alt}
     />
-  );
+  )
 }
 
-export default LazyImage;
+export default LazyImage
