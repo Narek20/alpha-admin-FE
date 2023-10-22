15a979586b65873d76c2abe93a70f3e6536192f1
Narek Hovhannisyan <n.hovo@bk.ru>
2023-10-22 11:47:35 +0400

    revert changes

--- src/components/shared/ImageCarousel/index.tsx
+++ src/components/shared/ImageCarousel/index.tsx
@@ -21,7 +21,7 @@ const ImageCarousel: FC<IProps> = ({ slides }) => {
           <img
             className={styles.carouselImg}
             key={slide}
-            src={process.env.REACT_APP_BASE_URL + slide}
+            src={slide}
             alt="նկար"
           />
         </div>
